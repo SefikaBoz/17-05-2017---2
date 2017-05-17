205e50440bb06e0830e380c3d8fee8205bbb4c7d
SefikaBoz <sefikaboz12@gmail.com>
2017-05-17 21:33:19 +0300

    Create Member.cs

---
+++ Member.cs
@@ -0,0 +1,25 @@
+# 17-05-2017---2
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Member
+    {
+        public string Ad { get; set; }
+        public Member()
+        {
+            Ad = "I am Member";
+            Console.WriteLine(Ad);
+        }
+        public void SendMessage()
+        {
+            Console.WriteLine("Message sent..");
+        }
+        
+    }
+}
